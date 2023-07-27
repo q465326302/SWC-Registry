dd1a7b73cc850c3a3180704678b516e305683e9b
zk20221025 <1036767219@qq.com>
2023-07-27 15:57:28 +0800

    SWC-114

---
+++ SWC-114/ERC20.sol
@@ -0,0 +1,121 @@
+pragma solidity ^0.4.24;
+
+/** Taken from the OpenZeppelin github
+ * @title SafeMath
+ * @使用安全检查的开发数学操作，出错时进行回滚。
+ */
+library SafeMath {
+
+  /**
+  * @开发者编写了一个函数，用于计算两个数字的乘积，当结果溢出时会进行反转。
+  */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    // 气体优化：这比要求 'a' 不为零要便宜，但如果还要测试 'b'，则会失去这个好处。
+    // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
+    if (a == 0) {
+      return 0;
+    }
+
+    uint256 c = a * b;
+    require(c / a == b);
+
+    return c;
+  }
+
+  /**
+  * @将两个数字进行整数除法，截断商，如果除数为零则返回。
+  */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    require(b > 0); // Solidity only automatically asserts when dividing by 0
+    uint256 c = a / b;
+    // 断言（assert）(a == b * c + a % b); // 在任何情况下都不会违反这个条件
+
+    return c;
+  }
+
+  /**
+  * @开发者将两个数字相减，如果被减数大于减数，则返回溢出。
+  */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    require(b <= a);
+    uint256 c = a - b;
+
+    return c;
+  }
+
+  /**
+  * @开发者将两个数字相加，在溢出时进行还原。
+  */
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    require(c >= a);
+
+    return c;
+  }
+
+  /**
+  * @dev函数用于两个数相除并返回余数（无符号整数模除），当除以零时返回错误。
+  */
+  function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+    require(b != 0);
+    return a % b;
+  }
+}
+
+
+contract ERC20 {
+
+  event Transfer( address indexed from, address indexed to, uint256 value );
+  event Approval( address indexed owner, address indexed spender, uint256 value);
+  using SafeMath for *;
+
+  mapping (address => uint256) private _balances;
+
+  mapping (address => mapping (address => uint256)) private _allowed;
+
+  uint256 private _totalSupply;
+
+  constructor(uint totalSupply){
+    _balances[msg.sender] = totalSupply;
+  }
+
+  function balanceOf(address owner) public view returns (uint256) {
+    return _balances[owner];
+  }
+
+
+  function allowance(address owner, address spender) public view returns (uint256)
+  {
+    return _allowed[owner][spender];
+  }
+
+  function transfer(address to, uint256 value) public returns (bool) {
+    require(value <= _balances[msg.sender]);
+    require(to != address(0));
+
+    _balances[msg.sender] = _balances[msg.sender].sub(value);
+    _balances[to] = _balances[to].add(value);
+    emit Transfer(msg.sender, to, value);
+    return true;
+  }
+
+  function approve(address spender, uint256 value) public returns (bool) {
+    require(spender != address(0));
+
+    _allowed[msg.sender][spender] = value;
+    emit Approval(msg.sender, spender, value);
+    return true;
+  }
+
+  function transferFrom(address from, address to, uint256 value) public returns (bool) {
+    require(value <= _balances[from]);
+    require(value <= _allowed[from][msg.sender]);
+    require(to != address(0));
+
+    _balances[from] = _balances[from].sub(value);
+    _balances[to] = _balances[to].add(value);
+    _allowed[from][msg.sender] = _allowed[from][msg.sender].sub(value);
+    emit Transfer(from, to, value);
+    return true;
+  }
+}
