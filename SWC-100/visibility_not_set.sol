20d2408dd2daee0e532c4596194b045bbbce75da
zk20221025 <1036767219@qq.com>
2023-07-31 09:47:52 +0800

    SWC-100

---
+++ SWC-100/visibility_not_set.sol
@@ -0,0 +1,20 @@
+/*
+ * @source: https://github.com/sigp/solidity-security-blog#visibility
+ * @author: SigmaPrime 
+ * Modified by Gerhard Wagner
+ */
+
+pragma solidity ^0.4.24;
+
+contract HashForEther {
+
+    function withdrawWinnings() {
+        // 如果地址的最后8个十六进制字符为0，则为赢家。 
+        require(uint32(msg.sender) == 0);
+        _sendWinnings();
+     }
+
+     function _sendWinnings() {
+         msg.sender.transfer(this.balance);
+     }
+}
